0428055b57da0ba0b618d5553ed2af28d694ba01
Metastasis <d1mq44@gmail.com>
2017-03-06 22:06:48 +0300

    Callback url support

--- lib/omniauth/strategies/headhunter.rb
+++ lib/omniauth/strategies/headhunter.rb
@@ -12,6 +12,9 @@ module OmniAuth
         :authorize_url => "/oauth/authorize"
       }
 
+      option :redirect_url
+
+
       uid { raw_info["id"].to_s }
 
       info do
@@ -29,6 +32,11 @@ module OmniAuth
         @raw_info ||= access_token.get(USERINFO_ENDPOINT).parsed
       end
 
+      private
+
+      def callback_url
+        options.redirect_url || (full_host + script_name + callback_path)
+      end
     end
   end
 end
